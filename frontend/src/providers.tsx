3fb16c8e25914a0e8aa81e4754e6520489792ee9
Joshua Hua <joshua.hua@coinbase.com>
2025-05-01 11:59:39 -0700

    init wallet

---
+++ frontend/src/providers.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { baseSepolia } from 'wagmi/chains';
+import { OnchainKitProvider } from '@coinbase/onchainkit';
+import type { ReactNode } from 'react';
+
+export function Providers(props: { children: ReactNode }) {
+  return (
+    <OnchainKitProvider
+      apiKey={import.meta.env.VITE_ONCHAINKIT_API_KEY}
+          chain={baseSepolia}
+          config={{ appearance: { 
+            mode: 'auto',
+        }
+      }}
+    >
+      {props.children}
+    </OnchainKitProvider>
+  );
+}
+
