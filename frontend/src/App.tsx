05ff61b5e0e4426761bb96cc815ec7f57b13f711
Griffin Sloves <griffinsloves@gmail.com>
2025-05-01 12:02:26 -0700

    Set up prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,23 @@
 // import { useState } from 'react'
-import './App.css'
+import './App.css';
 // import { useAccount } from 'wagmi';
 import { ConnectWallet } from '@coinbase/onchainkit/wallet';
 import { Wallet } from '@coinbase/onchainkit/wallet';
 import { Name } from '@coinbase/onchainkit/identity';
-import {
-  Avatar,
-} from '@coinbase/onchainkit/identity';
+import { Avatar } from '@coinbase/onchainkit/identity';
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Wallet>
-          <ConnectWallet>
+        <ConnectWallet>
           <Avatar className="h-6 w-6" />
           <Name />
-          </ConnectWallet>
+        </ConnectWallet>
       </Wallet>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
